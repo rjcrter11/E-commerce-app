e3cec626baae7fc7ab21a01c1293d73c6f5e790a
Rachel <rjcrter11@gmail.com>
2020-08-31 16:31:14 -0500

    add react-router

--- clothes-connect/src/App.js
+++ clothes-connect/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
+
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route component={HomePage} path='/' />
+      </Switch>
     </div>
   );
 }
