d750dbf37814d0b5679eac4d12964aa542822206
Rachel <rjcrter11@gmail.com>
2020-09-01 15:54:30 -0500

    finishes sign in handlesubmit method

--- clothes-connect/src/components/sign-in/sign-in.component.jsx
+++ clothes-connect/src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 import './sign-in.styles.scss'
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
     constructor() {
@@ -15,9 +15,18 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = e => {
+    handleSubmit = async e => {
         e.preventDefault()
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.log(error);
+        }
+
+
     }
 
     handleChange = e => {
