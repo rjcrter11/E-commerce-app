b2db04e80242ac12ee41310d7c5d7369a0d39386
Rachel <rjcrter11@gmail.com>
2020-09-01 15:49:51 -0500

    implements firebase utils, included ability to store authenticated users in db

--- clothes-connect/src/App.js
+++ clothes-connect/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
@@ -23,10 +23,25 @@ class App extends React.Component {
 
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = createUserProfileDocument(userAuth)
 
+          ; (await userRef).onSnapshot(snapShot => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+            console.log(this.state)
+          });
+
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+
+    });
   }
 
 
