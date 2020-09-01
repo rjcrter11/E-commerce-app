8d507ee0ad05f52ebbf16e99658de72d9da99aad
Rachel <rjcrter11@gmail.com>
2020-09-01 07:51:09 -0500

    adds shop page, collection preview component, collection item component

--- clothes-connect/src/App.js
+++ clothes-connect/src/App.js
@@ -4,20 +4,15 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact component={HomePage} path='/' />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
