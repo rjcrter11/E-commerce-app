9b6f2d24eb7feb9b90d9758c7e4734121fd61e24
Rachel <rjcrter11@gmail.com>
2020-09-01 08:09:39 -0500

    adds header component and links to home and shop page

--- clothes-connect/src/App.js
+++ clothes-connect/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact component={HomePage} path='/' />
         <Route path='/shop' component={ShopPage} />
