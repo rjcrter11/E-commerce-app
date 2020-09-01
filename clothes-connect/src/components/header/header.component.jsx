fabd1db16b1db80fac7a2ffa57bb0230e328f7ac
Rachel <rjcrter11@gmail.com>
2020-09-01 16:56:27 -0500

    adds redux, implements userReducer and userActions to header and app components

--- clothes-connect/src/components/header/header.component.jsx
+++ clothes-connect/src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.utils'
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -27,4 +28,8 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header 
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
