3bb1b323e3a5365242b8c086d624300dd34cbd99
Rachel <rjcrter11@gmail.com>
2020-09-02 10:12:50 -0500

    moves dir and shop data into it's own reducer, creates respective selectors and updates corresponding components with new redux flow for directory/shop

--- clothes-connect/src/components/collections-overview/collections-overview.component.jsx
+++ clothes-connect/src/components/collections-overview/collections-overview.component.jsx
@@ -3,22 +3,20 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview.component'
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollections, selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview' >
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
     </div>
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview); 
--- clothes-connect/src/pages/shop/shop.component.jsx
+++ clothes-connect/src/pages/shop/shop.component.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import CollectionPage from '../collection/collection.component'
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
     <div className='shop-page' >
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )
 
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
