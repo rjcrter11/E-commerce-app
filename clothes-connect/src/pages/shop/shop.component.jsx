988b936ee16a9b7c985c63130d3c1e9b468dcac7
Rachel <rjcrter11@gmail.com>
2020-09-02 08:37:31 -0500

    adds collections overview component

--- clothes-connect/src/pages/shop/shop.component.jsx
+++ clothes-connect/src/pages/shop/shop.component.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import { selectCollections } from '../../redux/shop/shop.selectors'
 
 const ShopPage = ({ collections }) => (
     <div className='shop-page' >
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
+        <CollectionsOverview />
     </div>
 )
 
