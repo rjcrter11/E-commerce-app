d30c1b6b55909004ba004f04cdc42272798dc168
Rachel <rjcrter11@gmail.com>
2020-10-13 17:43:40 -0500

    adds test for collecction page

--- clothes-connect/src/pages/collection/collection.component.jsx
+++ clothes-connect/src/pages/collection/collection.component.jsx
@@ -6,7 +6,7 @@ import { selectCollection } from '../../redux/shop/shop.selectors'
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+export const CollectionPage = ({ collection }) => {
     const { title, items } = collection;
     return (
         <div className='collection-page' >
