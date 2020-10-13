975f4d6de688365186838266002aa779b4d902fd
Rachel <rjcrter11@gmail.com>
2020-10-13 17:17:07 -0500

    adds tests for menu-item

--- clothes-connect/src/components/menu-item/menu-item.component.jsx
+++ clothes-connect/src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,13 @@ import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+export const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
 
     <div
+        data-testid="menu-item-container"
         className={`${size} menu-item`}
         onClick={() => history.push(`${match.url}${linkUrl}`)}  >
-        <div className='background-image' style={{
+        <div data-testid="background-image-container" className='background-image' style={{
             backgroundImage: `url(${imageUrl})`
         }} />
         <div className='content' >
