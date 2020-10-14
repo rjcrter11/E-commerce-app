b243c5cdcb49dab9373e6cb3e009136027db4bef
Rachel <rjcrter11@gmail.com>
2020-10-13 21:31:28 -0500

    adds snapshot test to app component

--- clothes-connect/src/App.test.js
+++ clothes-connect/src/App.test.js
@@ -1,3 +1,8 @@
-it("skip", () => {
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App'
 
-})
+it.skip("should render App component", () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper).toMatchSnapshot();
+});
